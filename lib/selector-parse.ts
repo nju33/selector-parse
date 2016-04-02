12abb2349f3b54c936904ee7dc45f28be3d575a8
nju33 <nju33@nmba.local>
2016-04-02 18:26:29 +0900

    Rewrite

---
+++ lib/selector-parse.ts
@@ -0,0 +1,59 @@
+interface Detail {
+  attr: string;
+  val: string;
+}
+
+function selectorParse(selector: string): any {
+  const re = /([#.[]?[^#.\[]+)/g;
+  const matches = selector.match(re);
+  if (matches) {
+    return traverse(matches);
+  }
+  return {};
+}
+
+function traverse(selectors: Array<string>): any {
+  const result: any = {};
+
+  selectors.forEach(selector => {
+    const detail: Detail = specify(selector);
+    if (detail.attr) {
+      if (!result[detail.attr]) {
+        result[detail.attr] = [];
+      }
+      result[detail.attr].push(detail.val);
+    } else {
+      result.tag = detail.val;
+    }
+  });
+
+  if (!result.tag) {
+    result.tag = 'div';
+  }
+  return result;
+}
+
+function specify(selector: string): Detail {
+  if (selector[0] === '#') {
+    const attr = 'id';
+    const val = selector.slice(1, selector.length);
+    return {attr, val};
+
+  } else if (selector[0] === '.') {
+    const attr = 'class';
+    const val = selector.slice(1, selector.length);
+    return {attr, val};
+
+  } else if (selector[0] === '[' && selector[selector.length - 1] === ']') {
+    const sliced: string = selector.slice(1, selector.length - 1);
+    const splited: Array<string> = sliced.split('=');
+    const attr = splited[0];
+    const val = splited[1];
+    return {attr, val};
+  }
+
+  return {
+    attr: null,
+    val: selector
+  };
+}
